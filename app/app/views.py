f64c6e6c960a3ccb8bc6eca01f2b339712f2f9de
dtiesling <tiesling@gmail.com>
2020-12-01 21:37:09 -0800

    Adds a second chart for trends over thelast 2 weeks.

--- app/app/views.py
+++ app/app/views.py
@@ -16,26 +16,39 @@ def index(request):
     date_series.drop(date_series.tail(1).index, inplace=True)
     case_count_series = dataset['Rancho Mission Viejo']
     case_count_series.drop(case_count_series.tail(1).index, inplace=True)
-    plot = figure(y_range=date_series,
+    plot = figure(title='Positive cases by day since beginning of pandemic.',
+                  y_range=date_series,
                   plot_width=800,
                   plot_height=len(date_series) * 18,
                   tools="save",
                   x_axis_label="Positive Cases Reported",
                   x_axis_location='above',
-                  x_minor_ticks=2,
-                  )
+                  x_minor_ticks=2)
     plot.hbar(y=date_series,
               right=case_count_series,
               left=0,
               height=0.4,
-              color="blue",
+              color='blue',
               fill_alpha=0.5,
               line_cap='round',
               hatch_alpha=0.0)
+    hbar_script, hbar_div = components(plot)
 
-    script, div = components(plot)
-    context = {'bokeh_script': script,
-               'bokeh_div': div,
+    line_plot = figure(x_range=date_series[-14:],
+                       plot_width=500,
+                       plot_height=300,
+                       tools="save,pan",
+                       title='Positive cases over the last 14 days.')
+    line_plot.xaxis.major_label_orientation = 45
+    line_plot.line(x=date_series,
+                   y=case_count_series)
+    line_script, line_div = components(line_plot)
+
+    context = {'hbar_script': hbar_script,
+               'hbar_div': hbar_div,
+               'line_script': line_script,
+               'line_div': line_div,
                'total_cases': case_count_series.sum(),
-               'last_updated': datetime.utcnow() - datetime.utcfromtimestamp(data_item.modified/1000)}
+               'last_updated': datetime.utcnow() - datetime.utcfromtimestamp(
+                   data_item.modified / 1000)}
     return render(request, 'app/index.html', context)
