cf034ecb173dc4bcb63da3b94e87e465657c3d64
dtiesling <tiesling@gmail.com>
2020-12-01 20:56:48 -0800

    Updates styling and adds more information.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM python:3.8
 
 RUN pip install pipenv
-
-COPY app /app
 COPY Pipfile /app/Pipfile
 COPY Pipfile.lock /app/Pipfile.lock
-
 WORKDIR /app
-
 RUN pipenv sync
-
+COPY app /app
 ENTRYPOINT [ "pipenv", "run", "gunicorn", "-b", ":8080", "app.wsgi" ]
--- app/app/views.py
+++ app/app/views.py
@@ -1,23 +1,41 @@
+from datetime import datetime
+
 import pandas as pd
+import time
 from arcgis import GIS
-from bokeh.embed import file_html
-from bokeh.plotting import figure, output_file, show
-from bokeh.resources import CDN
-from django.http import HttpResponse
+from bokeh.core.enums import LineCap
+from bokeh.embed import components
+from bokeh.plotting import figure
+from django.shortcuts import render
 
 
-def index(reqeuest):
+def index(request):
     data_item = GIS().content.get('772f5cdbb99c4f6689ed1460c26f4b05')
     dataset = pd.read_csv(data_item.get_data(try_json=False))
     date_series = dataset['DateSpecCollect']
     date_series.drop(date_series.tail(1).index, inplace=True)
-    # date_series = pd.to_datetime(date_series)
     case_count_series = dataset['Rancho Mission Viejo']
     case_count_series.drop(case_count_series.tail(1).index, inplace=True)
-    plot = figure(y_range=date_series, plot_width=800, plot_height=len(date_series) * 18,
-               title=f"RMV Case Count By Day: {case_count_series.sum()} total", x_axis_label="Cases Reported",
-               tools="pan,box_select,zoom_in,zoom_out,save,reset",
-               x_axis_location='above')
-    plot.hbar(y=date_series, right=case_count_series, left=0, height=0.4, color="orange",
-              fill_alpha=0.5)
-    return HttpResponse(file_html(plot, CDN, 'RMV Daily Vid-19 Case'))
+    plot = figure(y_range=date_series,
+                  plot_width=800,
+                  plot_height=len(date_series) * 18,
+                  tools="save",
+                  x_axis_label="Positive Cases Reported",
+                  x_axis_location='above',
+                  x_minor_ticks=2,
+                  )
+    plot.hbar(y=date_series,
+              right=case_count_series,
+              left=0,
+              height=0.4,
+              color="blue",
+              fill_alpha=0.5,
+              line_cap='round',
+              hatch_alpha=0.0)
+
+    script, div = components(plot)
+    context = {'bokeh_script': script,
+               'bokeh_div': div,
+               'total_cases': case_count_series.sum(),
+               'last_updated': datetime.utcnow() - datetime.utcfromtimestamp(data_item.modified/1000)}
+    return render(request, 'app/index.html', context)
